01580a97285cf189d2206f492d9abb9793b71f76
Richard B Gullo <richard_gullo@knights.ucf.edu>
2021-09-10 20:40:31 -0400

    Node Read/Write
    Added in zip functionality, read/write, and export functionality.

--- autograder-electron-project/main.js
+++ autograder-electron-project/main.js
@@ -1,5 +1,5 @@
-const {app, BrowserWindow, session} = require('electron');
-
+const {app, BrowserWindow, ipcMain, MenuItem, dialog} = require('electron');
+const AdmZip = require('adm-zip');
 let mainWindow;
 
 function createWindow () {
@@ -19,7 +19,53 @@ function createWindow () {
 
   mainWindow.loadFile('index.html');
 
+  mainWindow.webContents.openDevTools();
+
 }
+
+ipcMain.on('file-dropDown', (e, args) => {
+
+  const zip = new AdmZip(args);
+
+  const data = zip.getEntries().map((data) => {
+    const container = {};
+    container["name"] = data.name;
+    container["body"] = data.getData().toString();
+    return container;
+  });
+
+  e.sender.send('file-dropDown-response', data);
+})
+
+ipcMain.on('export-file',(e,args,location) =>{
+  const zip = AdmZip();
+  console.log(args);
+  console.log(location);
+  const file = args;
+  dialog.showSaveDialog(mainWindow,
+    {
+      defaultPath: './test',
+      filters:[
+        {
+          "name":"zip",
+          "extensions":["zip"]
+        }
+      ]
+    }).then(result => {
+
+    if(!result.canceled){
+    
+      file.forEach(file => {
+          zip.addFile(file.name,file.body);
+      });
+
+      zip.writeZip(result.filePath);
+    
+    }
+      
+  });
+
+});
 
 app.on('ready', createWindow);
 
